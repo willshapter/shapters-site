c2fca62b7af8f53d09ee3a2e4d959a0561a9ae53
willshapter <willshapter@gmail.com>
2026-07-20 15:03:23 -0300

    Update collections.ts

--- lib/collections.ts
+++ lib/collections.ts
@@ -5,12 +5,45 @@ export type Collection = {
 }
 
 export const collections: Collection[] = [
-  { title: "Sala De Jantar", image: "https://images.unsplash.com/photo-1617806118233-18e1c0c663bc?w=150&auto=format&fit=crop&q=80", href: "#" },
-  { title: "Presentes Para Ele", image: "https://images.unsplash.com/photo-1507679799987-c73779587ccf?w=150&auto=format&fit=crop&q=80", href: "#" },
-  { title: "Presentes Para Ela", image: "https://images.unsplash.com/photo-1513364776144-60967b0f800f?w=150&auto=format&fit=crop&q=80", href: "#" },
-  { title: "Roupas Femininas", image: "https://images.unsplash.com/photo-1434389677669-e08b4cac3105?w=150&auto=format&fit=crop&q=80", href: "#" },
-  { title: "Cuidados Com A Pele", image: "https://images.unsplash.com/photo-1556228720-195a672e8a03?w=150&auto=format&fit=crop&q=80", href: "#" },
-  { title: "Brinquedos", image: "https://images.unsplash.com/photo-1596461404969-9ae70f2830c1?w=150&auto=format&fit=crop&q=80", href: "#" },
-  { title: "Decoração De Jardim", image: "https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?w=150&auto=format&fit=crop&q=80", href: "#" },
-  { title: "Ideias De Presentes", image: "https://images.unsplash.com/photo-1549465220-1a8b9238cd48?w=150&auto=format&fit=crop&q=80", href: "#" },
+  { 
+    title: "Sala De Jantar", 
+    image: "https://images.unsplash.com/photo-1615529162924-f8605388461d?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
+  { 
+    title: "Presentes Para Ele", 
+    image: "https://images.unsplash.com/photo-1507679799987-c73779587ccf?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
+  { 
+    title: "Presentes Para Ela", 
+    image: "https://images.unsplash.com/photo-1513364776144-60967b0f800f?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
+  { 
+    title: "Roupas Femininas", 
+    image: "https://images.unsplash.com/photo-1434389677669-e08b4cac3105?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
+  { 
+    title: "Cuidados Com A Pele", 
+    image: "https://images.unsplash.com/photo-1556228720-195a672e8a03?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
+  { 
+    title: "Brinquedos", 
+    image: "https://images.unsplash.com/photo-1596461404969-9ae70f2830c1?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
+  { 
+    title: "Decoração De Jardim", 
+    image: "https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
+  { 
+    title: "Ideias De Presentes", 
+    image: "https://images.unsplash.com/photo-1549465220-1a8b9238cd48?w=300&auto=format&fit=crop&q=80", 
+    href: "#" 
+  },
 ]
+    
