9f4ee6277b345ed4ee1059efc9fe8f773deb8684
willshapter <willshapter@gmail.com>
2026-07-21 06:09:40 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { SiteHeader } from "@/components/site-header"
 import { CollectionsSection } from "@/components/collections-section"
-import { ShippingBanner } from "@/components/shipping-banner" // Componente das faixas azuis
+import { ShippingBanner } from "@/components/shipping-banner"
 import { ProductGrid } from "@/components/product-grid"
 import { Footer } from "@/components/site-footer"
 
@@ -10,13 +10,13 @@ export default function Home() {
       {/* 1. Cabeçalho / Menu */}
       <SiteHeader />
 
-      {/* 2. Seção de Coleções / Bolinhas (Temas) */}
-      <CollectionsSection />
-
-      {/* 3. As Faixas Azuis (Compromissos e Ofertas Relâmpago com Cronômetro) */}
+      {/* 2. Banner no Topo (Subiu para ficar em cima) */}
       <div className="mx-auto max-w-7xl w-full px-4 my-4">
         <ShippingBanner />
       </div>
+
+      {/* 3. Seção de Coleções / Bolinhas (Agora fica embaixo do banner) */}
+      <CollectionsSection />
 
       {/* 4. Grid de Produtos Populares */}
       <div className="flex-1">
